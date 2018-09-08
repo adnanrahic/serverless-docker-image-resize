e6df85e0537fad121cb4e6f20301342e431afb07
adnanrahic <ado.raha198@gmail.com>
2018-09-09 01:45:28 +0200

    some bash magic

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,13 @@ sls config credentials --provider aws --key ${SLS_KEY} --secret ${SLS_SECRET}
 # Deploy code
 sls deploy
 
+# find and replace the service endpoint
+sls info -v | grep ServiceEndpoint > serviceEndpoint.txt
+sed -i 's/ServiceEndpoint:\ //g' serviceEndpoint.txt
+serviceEndpoint=cat serviceEndpoint.txt
+# TODO: sed replace DOMAIN in secrets with the $serviceEndpoint var
+# code here...
+
 # Deploy domain
 sls create_domain
 
