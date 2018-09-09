7a0f2738de89c1f244c7cdf6e15d49960c3734a8
adnanrahic <ado.raha198@gmail.com>
2018-09-09 22:05:18 +0200

    build done; removed custom domain; TODO: add version with custom domain

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,24 @@
 cd /deploy
 
 # REPLACE `XYZ` WITH YOUR KEYS
-sls config credentials --provider aws --key ${SLS_KEY} --secret ${SLS_SECRET}
+sls config credentials --provider aws --key ${SLS_KEY} --secret ${SLS_SECRET} --profile serverless-admin
 
 # Deploy code
 sls deploy
 
 # find and replace the service endpoint
-sls info -v | grep ServiceEndpoint > serviceEndpoint.txt
-sed -i 's/ServiceEndpoint:\ //g' serviceEndpoint.txt
-serviceEndpoint=cat serviceEndpoint.txt
-# TODO: sed replace DOMAIN in secrets with the $serviceEndpoint var
-# code here...
+stage=${STAGE}
+if [ -z ${stage+dev} ]; then echo "stage is unset"; else echo "stage is set to '$stage'"; fi
+
+sls info -v | grep ServiceEndpoint > domain.txt
+sed -i 's@ServiceEndpoint:\ https:\/\/@@g' domain.txt
+sed -i "s@/${STAGE}@@g" domain.txt
+domain=$(cat domain.txt)
+sed -i "s@REPLACE_ME@$domain@g" secrets.json
+rm domain.txt
 
 # Deploy domain
-sls create_domain
+# sls create_domain
 
 # Deploy code again
 sls deploy
