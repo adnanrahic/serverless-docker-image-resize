a1f73678fad86e60882e3c5f0199501c49cce6d4
adnanrahic <ado.raha198@gmail.com>
2018-09-12 16:25:04 +0200

    wip: divided the services

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,9 @@ cd /deploy
 # REPLACE `XYZ` WITH YOUR KEYS
 sls config credentials --provider aws --key ${SLS_KEY} --secret ${SLS_SECRET} --profile serverless-admin
 
+# cd into functions dir
+cd /deploy/functions
+
 # Deploy code
 sls deploy
 
@@ -14,14 +17,16 @@ sls info -v | grep ServiceEndpoint > domain.txt
 sed -i 's@ServiceEndpoint:\ https:\/\/@@g' domain.txt
 sed -i "s@/${STAGE}@@g" domain.txt
 domain=$(cat domain.txt)
-sed -i "s@REPLACE_ME@$domain@g" secrets.json
+sed -i "s@REPLACE_ME@$domain@g" ../secrets.json
 rm domain.txt
+
+cd /deploy/bucket
+
+# Deploy bucket config
+sls deploy
 
 # Deploy domain
 # sls create_domain
-
-# Deploy code again
-sls deploy
 
 echo "------------------"
 echo "Service deployed. Press CTRL+C to exit."
